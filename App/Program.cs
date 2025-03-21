6ae3ac112d449185a09bf148bf1cf32e51e7de23
Jonathon Martin <jonathon.martin@cityelectricsupply.com>
2025-03-21 11:02:23 -0700

    App

---
+++ App/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.WriteLine("How many roads must a man walk down?");
+var x = Console.ReadLine() ?? "";
+if (x.ToLower().Trim() == "42")
+    Console.WriteLine("Correct! It is 42.");
+else
+    Console.WriteLine("Incorrect! It is 42.");
+
